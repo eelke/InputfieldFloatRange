9218dd162aa2ec2d14262d3b4d42878ba53606f7
Eelke Feenstra <dev@eelke.net>
2023-02-02 13:08:21 +0100

    Fix bug where InputfieldFloat negative precision prevented the displayed value to be updated properly Revert installs & requires, so direct installs from Modules Directory (should) work
    Signed-off-by: Eelke Feenstra <dev@eelke.net>

--- InputfieldFloatRange.js
+++ InputfieldFloatRange.js
@@ -11,30 +11,32 @@ function getSettings(id) {
     return window.ProcessWire.config.InputfieldFloatRange[id];
 }
 
-var $ = window.$;
 
-$(document).ready(function() {
+const { $ } = window;
+
+$(document).ready(function () {
 
     // Display range slider value in textfield
-    $(document).on("change input", ".InputfieldFloatRange", function(event) {
+    $(document).on("change input", ".InputfieldFloatRange", function (event) {
 
         // Prevent weird issue where InputfieldFloatRange__display's changes are also processed
         if ($(event.target).hasClass("InputfieldFloatRange__display")) return;
-
-        var settings = getSettings(event.target.id);
-        if (settings.displayValueField){
-            event.target.nextSibling.value = parseFloat(event.target.value).toFixed(settings.precision);
+        const settings = getSettings(event.target.id);
+        const precision = Math.min(20, Math.max(0, settings.precision));
+        if (settings.displayValueField) {
+            event.target.nextSibling.value = parseFloat(event.target.value).toFixed(precision);
         }
     });
 
     // Forward changes in textfield to range slider
-    $(document).on("input", ".InputfieldFloatRange__display", function(event) {
+    $(document).on("input", ".InputfieldFloatRange__display", function (event) {
 
-        var t = event.target;
+        const t = event.target;
 
-        var ranger = t.previousSibling;
-        var settings = getSettings(ranger.id);
-        var minMaxValue = Math.min(settings.max, Math.max(settings.min, parseFloat(t.value).toFixed(settings.precision)));
+        const ranger = t.previousSibling;
+        const settings = getSettings(ranger.id);
+        const precision = Math.min(20, Math.max(0, settings.precision));
+        const minMaxValue = Math.min(settings.max, Math.max(settings.min, parseFloat(t.value).toFixed(precision)));
 
         if (!isNaN(minMaxValue)) {
             ranger.value = minMaxValue;
@@ -42,26 +44,27 @@ $(document).ready(function() {
     });
 
     // After leaving the field, make sure its value matches precision, min, max and step settings
-    $(document).on("blur", ".InputfieldFloatRange__display", function(event) {
+    $(document).on("blur", ".InputfieldFloatRange__display", function (event) {
 
-        var t = event.target;
-        var ranger = t.previousSibling;
-        var settings = getSettings(ranger.id);
+        const t = event.target;
+        const ranger = t.previousSibling;
+        const settings = getSettings(ranger.id);
 
         // Replace comma's with dots, if required
         // TODO: Format number according to locale?
-        var val = t.value.replace(/,/g, ".");
+        const val = t.value.replace(/,/g, ".");
 
         // Round to precision number of decimals
-        var minMaxValue = parseFloat(val).toFixed(settings.precision);
+        const precision = Math.min(20, Math.max(0, settings.precision));
+        let minMaxValue = parseFloat(val).toFixed(precision);
 
         // Fit number within min and max values
         if (settings.min !== "") minMaxValue = Math.max(settings.min, minMaxValue);
         if (settings.max !== "") minMaxValue = Math.min(settings.max, minMaxValue);
 
         // If there is a set step value, round value to nearest step
-        if (settings.step !== "" && settings.step !==  0 && settings.rounding !== "disabled"){
-            var step = parseFloat(settings.step);
+        if (settings.step !== "" && settings.step !== 0 && settings.rounding !== "disabled") {
+            const step = parseFloat(settings.step);
             if (settings.rounding === "round") minMaxValue = step * Math.round(minMaxValue / step);
             else if (settings.rounding === "floor") minMaxValue = step * Math.floor(minMaxValue / step);
             else if (settings.rounding === "ceil") minMaxValue = step * Math.ceil(minMaxValue / step);
